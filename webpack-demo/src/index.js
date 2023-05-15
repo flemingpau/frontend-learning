0be6e92bbd01b511de97332439fce925adfe6ce2
Fleming Pau <flemingpausuiyung@gmail.com>
2023-05-15 09:56:53 +0800

    output management

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,17 +1,15 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
+import printMe from './print.js';
 
 function component(){
 	const element=document.createElement('div');
+	const btn = document.createElement('button');
 	//lodash在当前script中使用import引入
 	element.innerHTML=_.join(['Hello','webpack'],' ');
-	element.classList.add('hello');
-	
-	const myIcon = new Image();
-	myIcon.src=Icon;
-	element.appendChild(myIcon);
-	
+	btn.innerHTML = 'Click me and check the console!';
+	btn.onclick = printMe;
+
+	element.appendChild(btn);
 	return element;
 }
 document.body.appendChild(component());
