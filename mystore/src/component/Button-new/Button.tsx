e550767506b9a5b3ede94b0ca7173aa0f5c80c99
Fleming Pau <flemingpausuiyung@gmail.com>
2023-05-29 10:27:25 +0800

    1

--- mystore/src/component/Button-new/Button.tsx
+++ mystore/src/component/Button-new/Button.tsx
@@ -1,9 +1,30 @@
 import React, { FunctionComponent } from "react";
 import { props } from "./props";
-type ButtonProps={
+interface ButtonProps{
     name:string,
     props:any,
 }
-const Button : FunctionComponent<{}> = props=>{props.children};
-
+const Button: FunctionComponent<ButtonProps> = (props) => {
+    const { btnType, className, disabled, size, href, children, ...restProps } =
+      props;
+    const classes = classNames("btn", className, {
+      [`btn-${btnType}`]: btnType,
+      [`btn-${size}`]: size,
+      disabled: btnType === "link" && disabled,
+    });
+    if (btnType === "link" && href) {
+      return (
+        <a className={classes} href={href} {...restProps}>
+          {children}
+        </a>
+      );
+    } else {
+      return (
+        <button className={classes} disabled={disabled} >
+          {children}
+        </button>
+      );
+    }
+  };
+  
 export default Button;
